0dc7b283743896ce03b3d07f86781fb56dfab086
Valentin Fonseca <113689631+ViliFonseca@users.noreply.github.com>
2025-06-18 23:36:24 -0300

    Comentarios finales

--- Greedy.java
+++ Greedy.java
@@ -4,6 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Greedy {
+	
+	/*Candidatos:Maquinas capaces de producir piezas.
+	 * Estrategia: la estrategia por la cual realiza el algoritmo, es de
+	 * ordenar todas las maquinas posibles por la cantidad de piezas posibles
+	 * que puede realizar cada maquina,al elegir la  maquina(Es decir
+	 * la mas grande en capacidad de produccion) que le permite acercarse
+	 * al total de piezas a producir, la va a reutilizar las cantidades de veces
+	 * que pueda hasta que no se lo permita, entonces ahi pasara a la siguiente maquina
+	 * mas capaz,al finalizar retornara, tambien al seleccionar una maquina el algoritmo sumara 
+	 * los candidatos que se han usado.
+	 * Cuando se haya llegado retornara Solucion que guarda la lista total de maquinas utilizadas y 
+	 * la cantidad de candidatos.
+	 * Al no encontrar solucion nuestro algoritmo envia la aproximacion mas cercana que pudo encontrar.
+	 */
     public static Solucion greedy(List<Maquina> maquinas, int piezasAProducir){
         //Se ordenan las maquinas de mayor a menor
         maquinas.sort((a,b)->b.getPiezas()-a.getPiezas());
